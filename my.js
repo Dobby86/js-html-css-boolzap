277690dd1bec3aea608bcb3267871b380860e860
marco <dobermannpoker86@gmail.com>
2020-04-07 11:12:29 +0200

    primo passo

---
+++ my.js
@@ -0,0 +1,5 @@
+console.log("ciao");
+// DESCRIZIONE:
+// Milestone 1:
+// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse (quindi tutto statico);
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando invia il testo viene aggiunto al thread sopra, come messaggio verde (quindi solo quello NON aggiungiamo dinamicamente anche quello bianco di risposta)
